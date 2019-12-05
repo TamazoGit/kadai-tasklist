53ede6953768a0fe4d1c7ba76cdb2367dd3c3c28
tamazo <t-tamayose@n-monitor.co.jp>
2019-12-05 04:29:55 +0000

    add columns kadai-tasklist

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -4,10 +4,17 @@
 
     <h1>タスク新規作成ページ</h1>
 
+    {!! Form::model($task, ['route' => 'Tasks.store']) !!}
+    
     <div class="row">
         <div class="col-6">
             {!! Form::model($task, ['route' => 'Tasks.store']) !!}
         
+                <div class="form-group">
+                    {!! Form::label('status', 'ステータス:') !!}
+                    {!! Form::text('status', null, ['class' => 'form-control']) !!}
+                </div>
+
                 <div class="form-group">
                     {!! Form::label('content', 'タスク:') !!}
                     {!! Form::text('content', null, ['class' => 'form-control']) !!}
